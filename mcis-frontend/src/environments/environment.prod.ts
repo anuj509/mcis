785471bc1b9ee77a2cf2efaa9df5399b6b5dcecd
Shah <anuj.shah@atos.net>
2019-03-27 16:33:38 +0530

    backend code

--- mcis-frontend/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-const baseUrl = 'http://localhost:8000/api';
-export const environment = {
-  production: true,
-  addManufacturerURL: baseUrl+'/add-manufacturer',
-  getManufacturersURL: baseUrl+'/get-manufacturers',
-  addModelURL: baseUrl+'/add-model',
-  viewInventoryURL: baseUrl+'/view-inventory',
-  markAsSoldURL: baseUrl+'/mark-as-sold'
-};
