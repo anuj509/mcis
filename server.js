e61873f12651d0905b81b848e1c4af344301541c
Anuj Shah <anujshah.2016@gmail.com>
2019-03-28 00:25:19 +0530

    server js changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/car-inventory-system'));
+app.use(express.static('./dist/mcis-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/car-inventory-system/index.html'));
+res.sendFile(path.join(__dirname,'/dist/mcis-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
