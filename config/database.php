395105325acb657bbfea605ec91eba435bc146e5
Anuj Shah <anujshah.2016@gmail.com>
2019-03-27 23:26:07 +0530

    update database

--- config/database.php
+++ config/database.php
@@ -42,12 +42,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'database' => env('DB_DATABASE', 'mcis_app'),
+            'username' => env('DB_USERNAME', 'mcis_root'),
+            'password' => env('DB_PASSWORD', 'mcis1234'),
+            'unix_socket' => env('DB_SOCKET', 'mcis1234'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
